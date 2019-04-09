8e6b028dfcdfb250be37c2d4a3f8d3662a6a4672
mgryszko <marcin.gryszko@lastminute.com>
2019-04-09 07:06:58 +0200

    Refactor - add in a loop (contract)

--- src/test/java/StringCalculator.java
+++ src/test/java/StringCalculator.java
@@ -6,21 +6,6 @@ class StringCalculator {
 
     if (numbers.contains(",")) {
       String[] addends = numbers.split(",");
-      int addend1 = Integer.valueOf(addends[0]);
-      int addend2 = Integer.valueOf(addends[1]);
-      int addend3 = 0;
-      if (addends.length == 3 || addends.length == 4) {
-        addend3 = Integer.valueOf(addends[2]);
-      }
-      int addend4  = 0;
-      if (addends.length == 4) {
-        addend4 = Integer.valueOf(addends[3]);
-      }
-      return addend1 + addend2 + addend3 + addend4;
-    }
-
-    if (numbers.contains(",")) {
-      String[] addends = numbers.split(",");
       int sum = 0;
       for (String number : addends) {
         int addend = Integer.valueOf(number);
