536050a9d3cca2ccd7ad06fb05ed79ba7e85a778
mgryszko <marcin.gryszko@lastminute.com>
2019-04-09 07:13:31 +0200

    Refactor - handle empty string in sum loop

--- src/test/java/StringCalculator.java
+++ src/test/java/StringCalculator.java
@@ -1,14 +1,18 @@
 class StringCalculator {
   public int add(String numbers) {
-    if (numbers.isBlank()) {
-      return 0;
-    }
-
     String[] addends = numbers.split(",");
     int sum = 0;
     for (String number : addends) {
-      sum += Integer.valueOf(number);
+      sum += valueOf(number);
     }
     return sum;
+  }
+
+  private Integer valueOf(String number) {
+    if (number.isEmpty()) {
+      return 0;
+    }
+
+    return Integer.valueOf(number) ;
   }
 }
