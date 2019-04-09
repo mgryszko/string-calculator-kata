6dbc72c55e33e1d0f86c462db3660e0592742558
mgryszko <marcin.gryszko@lastminute.com>
2019-04-09 06:49:59 +0200

    Refactor - replace constant with computed values

--- src/test/java/StringCalculator.java
+++ src/test/java/StringCalculator.java
@@ -4,10 +4,14 @@ class StringCalculator {
       return 0;
     }
     if (numbers.equals("1,2")) {
-      return 1 + 2;
+      int addend1 = Integer.valueOf(numbers.split(",")[0]);
+      int addend2 = Integer.valueOf(numbers.split(",")[1]);
+      return addend1 + addend2;
     }
     if (numbers.equals("2,4")) {
-      return 6;
+      int addend1 = Integer.valueOf(numbers.split(",")[0]);
+      int addend2 = Integer.valueOf(numbers.split(",")[1]);
+      return addend1 + addend2;
     }
     return Integer.valueOf(numbers);
   }
